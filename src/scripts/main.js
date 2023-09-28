4019baa1251260ad68317ecc61a9bc41867b0e25
ArvinAjifTechology <arvinajif3@gmail.com>
2023-09-28 13:55:38 +0700

    XHR

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,5 @@
 function main() {
+  const baseUrl = "https://books-api.dicoding.dev";
   const getBook = () => {
     // tuliskan kode di sini!
     // membuat instance dari XMLHttpRequest
@@ -20,7 +21,7 @@ function main() {
     };
 
     // Membuat GET request dan menetapkan target URL
-    xhr.open("GET", "https://books-api.dicoding.dev/list");
+    xhr.open("GET", `${baseUrl}/list`);
 
     // Mengirimkan request
     xhr.send();
@@ -51,7 +52,7 @@ function main() {
     };
 
     // Membuat POST request dan menetapkan target URL
-    xhr.open("POST", "https://books-api.dicoding.dev/add");
+    xhr.open("POST", `${baseUrl}/add`);
 
     // Mementapkan properti Content-Type dan X-Auth-Token pada Header request
     xhr.setRequestHeader("Content-Type", "application/json");
@@ -63,10 +64,55 @@ function main() {
 
   const updateBook = (book) => {
     // tuliskan kode di sini!
+    // Membuat instance dari XMLHttpRequest
+    const xhr = new XMLHttpRequest();
+
+    //menetapkan callback jika response sukses dan error
+    xhr.onload = function () {
+      const responseJson = JSON.parse(this.responseText);
+      showResponseMessage(responseJson.message);
+      getBook();
+    };
+
+    xhr.onerror = function () {
+      showResponseMessage();
+    };
+
+    // Membuat PUT request dan menetapkan target URL
+    xhr.open("PUT", `${baseUrl}/edit/${book.id}`);
+
+    // Mementapkan properti Content-Type dan X-Auth-Token pada Header request
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("X-Auth-Token", "12345");
+
+    // Mengirimkan request dan menyisipkan JSON.stringify(book) pada body
+    xhr.send(JSON.stringify(book));
   };
 
   const removeBook = (bookId) => {
     // tuliskan kode di sini!
+    // Membuat instance dari XMLHttpRequest
+  const xhr = new XMLHttpRequest();
+ 
+  //menetapkan callback jika response sukses dan error
+  xhr.onload = function () {
+    const responseJson = JSON.parse(this.responseText);
+    showResponseMessage(responseJson.message);
+    getBook();
+  };
+ 
+  xhr.onerror = function () {
+    showResponseMessage();
+  };
+ 
+  // Membuat DELETE request dan menetapkan target URL
+  xhr.open('DELETE', `${baseUrl}/delete/${bookId}`);
+ 
+  // Mementapkan properti Content-Type dan X-Auth-Token pada Header request
+  xhr.setRequestHeader('X-Auth-Token', '12345');
+ 
+  // Mengirimkan request
+  xhr.send();
   };
 
   /*
